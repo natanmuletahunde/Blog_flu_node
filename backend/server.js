69b384893a78533020a4c5d6334fbd15320a24d2
pivnati <natanmuleta911@gmail.com>
2025-05-26 16:44:32 -0700

    create the .env file

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,7 @@ app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/blogApp', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
