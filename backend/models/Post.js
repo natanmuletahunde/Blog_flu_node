1a5f51bcd5ef516648dc48d33e1f50090ae6a754
pivnati <natanmuleta911@gmail.com>
2025-05-26 16:05:21 -0700

    modfiy thepost.js

---
+++ backend/models/Post.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Post', postSchema);
